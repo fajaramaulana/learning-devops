c9cddb18910c35eaaebab9caf82d26821a1f3648
fajaramaulana <fajar.maulana@indocyber.id>
2025-02-07 08:18:19 +0700

    Enhance restart script to securely fetch public IP and handle errors

--- restart-docker.sh
+++ restart-docker.sh
@@ -2,8 +2,15 @@ echo "Stopping Docker Compose services..."
 docker-compose down
 
 echo "Fetching latest public IP..."
-export PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+export PUBLIC_IP=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
+
 echo "Current Public IP: $PUBLIC_IP"
+
+if [ -z "$PUBLIC_IP" ]; then
+  echo "Failed to fetch public IP. Exiting."
+  exit 1
+fi
 
 echo "Starting Docker Compose services..."
 docker-compose up -d
